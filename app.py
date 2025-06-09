b870a21602ecb68f6884b1c16dc72e0252ca415a
amitiz <sthaamit63@gmail.com>
2025-06-09 19:17:24 +1000

    add

--- app.py
+++ app.py
@@ -1,7 +1,7 @@
 import streamlit as st
 import pandas as pd
 
-st.set_page_config(page_title="Amit App", layout="wide")
+st.set_page_config(page_title="Amit", layout="wide")
 
 file=pd.read_csv("data/amazon.csv")
 
