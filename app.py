bb83ff56d23560ff08ffbaa7712bf2e96604bc40
amitiz <sthaamit63@gmail.com>
2025-06-09 19:28:24 +1000

    chart

--- app.py
+++ app.py
@@ -3,7 +3,11 @@ import pandas as pd
 
 st.set_page_config(page_title="Amit", layout="wide")
 
-file=pd.read_csv("data/amazon.csv")
+file = st.file_uploader("Upload CSV", type="csv")
 
-st.write("Hello World!")
-st.write(file)
+if file:
+    df = pd.read_csv(file)
+    st.write(df.head())
+    st.bar_chart(df.iloc[8])
+else:
+    st.write("please upload file")
