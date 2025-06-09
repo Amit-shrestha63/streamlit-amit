dc3868e7d2fd3edff6c196061f29b06061147900
amitiz <sthaamit63@gmail.com>
2025-06-09 19:15:29 +1000

    amit add csv file

--- app.py
+++ app.py
@@ -1,4 +1,9 @@
 import streamlit as st
+import pandas as pd
 
+st.set_page_config(page_title="Amit App", layout="wide")
+
+file=pd.read_csv("data/amazon.csv")
 
 st.write("Hello World!")
+st.write(file)
